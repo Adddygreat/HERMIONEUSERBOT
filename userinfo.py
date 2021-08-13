3bda90d857619a0faf267eba673c7cb10063fde3
Adddygreat <88867924+Adddygreat@users.noreply.github.com>
2021-08-13 11:10:39 +0530

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -9,8 +9,8 @@
 from telethon.events import NewMessage
 from typing import Union
 
-from HERMIONE import CMD_HELP
-from HERMIONE.events import register
+from ULTRA import CMD_HELP
+from ULTRA.events import register
 
 from re import findall, match
 from typing import List
